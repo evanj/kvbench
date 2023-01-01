f1ea84a454b8d8446017d582430ab41830c09294
Evan Jones <ej@evanjones.ca>
2023-01-01 11:37:31 -0500

    initial version that barely works

---
+++ Makefile
@@ -0,0 +1,21 @@
+all:
+	cargo fmt
+	cargo test
+	cargo check
+	# https://zhauniarovich.com/post/2021/2021-09-pedantic-clippy/#paranoid-clippy
+	# -D clippy::restriction is way too "safe"/careful
+	# -D clippy::pedantic is also probably too safe
+	cargo clippy --all-targets --all-features -- \
+		-D warnings \
+		-D clippy::nursery \
+		-D clippy::pedantic \
+		-A clippy::cast_precision_loss
+
+		# pedantic allows from another project
+		
+		#-A clippy::cast-sign-loss \
+		#-A clippy::cast-possible-truncation \
+		#-A clippy::too-many-lines \
+		#-A clippy::missing-panics-doc \
+		#-A clippy::missing-errors-doc \
+		#-A clippy::len-without-is-empty
---
+++ src/main.rs
@@ -0,0 +1,178 @@
+use rand::prelude::Distribution;
+use rand::SeedableRng;
+use std::collections::HashMap;
+use std::time::{Duration, Instant};
+
+#[derive(argh::FromArgs)]
+/// Configuration for the key/value benchmark.
+struct BenchmarkConfig {
+    /// number of keys.
+    #[argh(option, default = "10")]
+    num_keys: usize,
+
+    /// measurement duration.
+    #[argh(
+        option,
+        default = "Duration::from_secs(2)",
+        from_str_fn(argh_parse_go_duration)
+    )]
+    measure_duration: Duration,
+}
+
+// Parses a duration using Go's formats, with the signature required by argh.
+fn argh_parse_go_duration(s: &str) -> Result<Duration, String> {
+    let result = go_parse_duration::parse_duration(s);
+    match result {
+        Err(err) => Err(format!("{err:?}")),
+        Ok(nanos) => {
+            assert!(nanos >= 0);
+            Ok(Duration::from_nanos(
+                nanos.try_into().expect("BUG: duration must be >= 0"),
+            ))
+        }
+    }
+}
+
+#[derive(Debug)]
+struct KVError {
+    message: String,
+}
+
+impl KVError {
+    fn msg<T>(m: &str) -> Result<T, Self> {
+        let err = Self {
+            message: String::from(m),
+        };
+        Err(err)
+    }
+}
+
+trait KVStore {
+    fn put(&mut self, key: &[u8], value: &[u8]) -> Result<(), KVError>;
+    // fn get(&mut self, key: &[u8]) -> Result<&[u8], KVError>;
+}
+
+struct HashMapStore {
+    store: HashMap<Vec<u8>, Vec<u8>>,
+}
+
+impl HashMapStore {
+    fn new() -> Self {
+        Self {
+            store: HashMap::new(),
+        }
+    }
+}
+
+impl KVStore for HashMapStore {
+    fn put(&mut self, key: &[u8], value: &[u8]) -> Result<(), KVError> {
+        // TODO: avoid allocating the key on overwrite?
+        let key_vec = Vec::from(key);
+        let value_vec = Vec::from(value);
+        self.store.insert(key_vec, value_vec);
+        Ok(())
+    }
+}
+
+struct KeyGenerator {
+    rng: rand_xoshiro::Xoshiro256Plus,
+    num_keys: usize,
+    key_buffer: [u8; 8],
+    key_range: rand::distributions::Uniform<u64>,
+}
+
+impl KeyGenerator {
+    fn new(num_keys: usize) -> Self {
+        // the rand book suggests Xoshiro256Plus is fast and pretty good:
+        // https://rust-random.github.io/book/guide-rngs.html
+        let rng = rand_xoshiro::Xoshiro256Plus::from_entropy();
+        Self {
+            rng,
+            num_keys,
+            key_buffer: [0u8; 8],
+            key_range: rand::distributions::Uniform::from(0..num_keys as u64),
+        }
+    }
+
+    fn next_key(&mut self) -> &[u8] {
+        let key = self.key_range.sample(&mut self.rng) * 2;
+        self.key_buffer = key.to_be_bytes();
+        &self.key_buffer[..]
+    }
+}
+
+fn fill_store(store: &mut dyn KVStore, num_keys: usize) -> Result<(), KVError> {
+    println!("filling store with {num_keys} keys ...");
+
+    let mut key_buffer: [u8; 8];
+    let start = Instant::now();
+    for i in 0..num_keys {
+        let k = i as u64 * 2;
+        key_buffer = k.to_be_bytes();
+        // println!("put i={i} k={k} bytes={:x?}", &key_buffer[..]);
+        let key_slice = &key_buffer[..];
+        store.put(key_slice, key_slice)?;
+    }
+    let end = Instant::now();
+    let duration = end - start;
+    println!(
+        "filled in {duration:?} ; {:.1} keys/sec",
+        num_keys as f64 / duration.as_secs_f64()
+    );
+
+    Ok(())
+}
+
+fn run_bench(
+    store: &mut dyn KVStore,
+    key_gen: &mut KeyGenerator,
+    measure_duration: Duration,
+) -> Result<(), KVError> {
+    let mut requests = 0usize;
+    let start = Instant::now();
+    let measure_end = start + measure_duration;
+
+    loop {
+        let now = Instant::now();
+        if now >= measure_end {
+            break;
+        }
+
+        requests += 1;
+        let key_slice = key_gen.next_key();
+        store.put(key_slice, key_slice)?;
+    }
+
+    let end = Instant::now();
+    let duration = end - start;
+    println!(
+        "{} requests in {duration:?}; {} requests/sec",
+        requests,
+        requests as f64 / duration.as_secs_f64()
+    );
+
+    Ok(())
+}
+
+fn main() -> Result<(), KVError> {
+    let config: BenchmarkConfig = argh::from_env();
+    println!(
+        "running benchmark num_keys={} measure_duration={:?}",
+        config.num_keys, config.measure_duration
+    );
+
+    let mut store = HashMapStore::new();
+    fill_store(&mut store, config.num_keys)?;
+
+    let mut key_gen = KeyGenerator::new(config.num_keys);
+    run_bench(&mut store, &mut key_gen, config.measure_duration)?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_stuff() {
+        assert_eq!(1, 1);
+    }
+}
